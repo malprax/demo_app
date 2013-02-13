02b73e69d7efc23624af3c5eba9030cf0d53d1e8
aulia sabril <kingmalprax@gmail.com>
2013-02-13 12:09:46 +0800

    sudah ada update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  validates :name, :presence => true, :length => {:maximum => 30}
+  validates :email, :presence => true, :length => {:maximum => 30}
   has_many :microposts
 end
